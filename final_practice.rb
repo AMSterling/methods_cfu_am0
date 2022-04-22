f9c45fa3e78c59b86c0b584e5c22fe42d1b16f7d
Anna Marie Sterling <sterling.am@gmail.com>
2022-04-21 21:55:54 -0600

    Initial commit

--- final_practice.rb
+++ final_practice.rb
@@ -1,38 +1,57 @@
 # Defining Our Own Methods
+#Worked with Bryce
 # 1: Write a method named greeting that prints out a general greeting to someone
+def greeting
+  puts "Hey"
+end
 
+greeting
 # What is the return value of your method?
+# => Hey
 # How many arguments did you pass your method?
-
-
+#0
 
 
 #2: Write a method named custom_greeting that prints out a greeting WITH a specific name.
+def custom_greeting (name1)
+  puts "Hello " + name1
+end
 
-
+custom_greeting("Bryce")
 # What is the return value of your method?
+# => Hello Bryce
 # How many arguments did you pass your method?
+#1
 # What data type was your argument(s)?
+#String
 
 
 
 
 #3: Write a method named square that takes in one number, and returns the square of that number
+def square (num)
+  puts num * num
+end
 
-
-
+square(9)
 # What is the return value of your method?
+# => 81
 # How many arguments did you pass your method?
+# 1
 # What data type was your argument(s)?
-
+#Integer
 
 
 
 #4: Write a method named full name that takes in 3 strings, a first, middle, and last name, and print outs the sentence of the entire string
+def full_name (first, middle, last)
+  puts "It is so nice to work with you " + first + middle + last
+end
 
-
-
+full_name("Anna ", "Marie ", "Sterling ")
 # What is the return value of your method?
+# => It is so nice to work with you Anna Marie Sterling
 # How many arguments did you pass your method?
+#1
 # What data type was your argument(s)?
-
+#String
